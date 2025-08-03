666e58e13056b91f5b25d004a6ebccbe0ac038f4
Kalyanighule123 <ghulekalyani028@gmail.com>
2025-08-03 09:18:51 +0530

    Add files via upload

---
+++ AdditionOfTwoNumbers.java
@@ -0,0 +1,8 @@
+public class AdditionOfTwoNumbers{
+    public static void main(String[] args)
+    {
+        int a=90;
+        int b=80;
+        System.out.println("Addition is"+  "  " +(a+b));
+    }
+}
