562c015c6ea8c7323901b2db3b730fa8a1d349bd
Kalyanighule123 <ghulekalyani028@gmail.com>
2025-08-03 09:19:34 +0530

    Add files via upload

---
+++ SubstractionOfTwoNumbers.java
@@ -0,0 +1,8 @@
+public class SubstractionOfTwoNumbers{
+    public static void main(String[] args)
+    {
+        int a=90;
+        int b=80;
+        System.out.println("Substraction is"+  "  " +(a-b));
+    }
+}
