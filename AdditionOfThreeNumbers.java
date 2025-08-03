b5fed44a7b4309a20f21675832546e9ed1a41f65
Kalyanighule123 <ghulekalyani028@gmail.com>
2025-08-03 09:19:14 +0530

    Add files via upload

---
+++ AdditionOfThreeNumbers.java
@@ -0,0 +1,9 @@
+public class AdditionOfThreeNumbers{
+    public static void main(String[] args)
+    {
+        int a=90;
+        int b=80;
+        int c=56;
+        System.out.println("Addition is"+  "  " +(a+b+c));
+    }
+}
