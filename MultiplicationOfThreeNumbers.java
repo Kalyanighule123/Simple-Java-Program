2e31c37126cf5fa7e7098d9f49d4f2f7a3e8d3f5
Kalyanighule123 <ghulekalyani028@gmail.com>
2025-08-04 22:07:43 +0530

    Add files via upload

---
+++ MultiplicationOfThreeNumbers.java
@@ -0,0 +1,9 @@
+public class MultiplicationOfThreeNumbers{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=80;
+        int c=56;
+        System.out.println("Multiplication is"+  "  " +(a*b*c));
+    }
+}
