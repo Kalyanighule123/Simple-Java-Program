c23f9ba9fccdf4c1307e346ee4cbf2f2b1b93677
Kalyanighule123 <ghulekalyani028@gmail.com>
2025-08-04 22:07:00 +0530

    Add files via upload

---
+++ SubstractionOfThreeNumbers.java
@@ -0,0 +1,9 @@
+public class SubstractionOfThreeNumbers{
+    public static void main(String[] args)
+    {
+        int a=190;
+        int b=80;
+        int c=56;
+        System.out.println("Substraction is"+  "  " +(a-b-c));
+    }
+}
