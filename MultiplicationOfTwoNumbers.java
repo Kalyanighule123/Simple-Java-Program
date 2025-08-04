9e45abe8f508f4c829b230de56aad6f614287c6f
Kalyanighule123 <ghulekalyani028@gmail.com>
2025-08-04 22:07:21 +0530

    Add files via upload

---
+++ MultiplicationOfTwoNumbers.java
@@ -0,0 +1,8 @@
+public class MultiplicationOfTwoNumbers{
+    public static void main(String[] args)
+    {
+        int a=9;
+        int b=8;
+        System.out.println("Multiplication is"+  "  " +(a*b));
+    }
+}
