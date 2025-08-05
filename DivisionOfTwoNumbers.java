fdd8719263d48432a9768c73f61fbd1b99c1fa7d
Kalyanighule123 <ghulekalyani028@gmail.com>
2025-08-05 19:22:24 +0530

    Add files via upload

---
+++ DivisionOfTwoNumbers.java
@@ -0,0 +1,8 @@
+public class DivisionOfTwoNumbers{
+    public static void main(String[] args)
+    {
+        int a=25;
+        int b=5;
+        System.out.println("Division is"+  "  " +(a/b));
+    }
+}
